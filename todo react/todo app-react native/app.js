3ca092607f874c507468ae9b7cf3d6917ceec930
Manaswinich <chinthalapudimanaswini.7@msitprogram.net>
2020-05-15 22:31:06 +0530

    application start

---
+++ todo react/todo app-react native/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import main from './app/main'
+
+export default class app extends React.Component {
+    render() {
+        return (
+            <main />
+        );
+    }
+}
