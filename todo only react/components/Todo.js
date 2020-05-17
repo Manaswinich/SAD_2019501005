b892b0a5c9e0ec2f5346d5ce0e884a58fd3c1a39
Manaswinich <chinthalapudimanaswini.7@msitprogram.net>
2020-05-18 01:33:10 +0530

    active,complete or all to be shown

--- todo only react/components/Todo.js
+++ todo only react/components/Todo.js
@@ -1,22 +1,63 @@
 import React from "react";
 import TodoForm from "./TodoForm";
+import Strike from './Strike';
 
 export default class Todo extends React.Component {
     state = {
-        todos: []
+        todos: [],
+        toShow: "all"
     };
     addTodo = todo => {
         this.setState({
             todos: [todo, ...this.state.todos]
         });
     };
+
+    toggleComplete = id => {
+        this.setState({
+            todos: this.state.todos.map(todo => {
+                if (todo.id === id) {
+                    return {
+                        // id: todo.id,
+                        // text: todo.text,
+                        ...todo,
+                        complete: !todo.complete
+                    };
+                } else {
+                    return todo;
+                }
+            })
+        });
+    }
+
+    updateToShow = (s) => {
+        this.setState({
+            toShow: s
+        })
+    }
+
     render() {
+        let todos = []
+
+        if (this.state.toShow === 'all') {
+            todos = this.state.todos;
+        } else if (this.state.toShow === 'active') {
+            todos = this.state.todos.filter(todo => !todo.complete);
+        } else if (this.state.toShow === 'complete') {
+            todos = this.state.todos.filter(todo => todo.complete);
+        }
         return (
             <div>
                 <TodoForm onSubmit={this.addTodo} />
-                {this.state.todos.map(todo => (
-                    <div key={todo.id}>{todo.text}</div>
+                {todos.map(todo => (
+                    <Strike key={todo.id} toggleComplete={() => this.toggleComplete(todo.id)} todo={todo} />
                 ))}
+                <div>Tasks left:{this.state.todos.filter(todo => !todo.complete).length}</div>
+                <div>
+                    <button onClick={() => this.updateToShow("all")}>all</button>
+                    <button onClick={() => this.updateToShow("active")}>active</button>
+                    <button onClick={() => this.updateToShow("complete")}>complete</button>
+                </div>
             </div>
         );
     }
